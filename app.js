18bae05a53c30b243607f1d22ecd9c157f1e4f44
ASR <ayushsinghrajput2020@gmail.com>
2024-03-10 21:04:37 +0530

    sidebar

---
+++ app.js
@@ -0,0 +1,15 @@
+let bars=document.querySelector(".sidebar")
+let tabs=document.querySelector(".bar");
+
+
+tabs.addEventListener("click", function ()  {
+    console.log("hello");
+          
+    
+})
+
+
+
+
+
+
